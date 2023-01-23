a4185ab072fa9a7663c829de2d965a2b121bdd79
RomanPolakov <113889059+RomanPolakov@users.noreply.github.com>
2023-01-23 11:08:19 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите колличества целых чисел в цикле и число b, на которое делить сумму чисель массива");
+int b = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = i;
+    sum = sum + arr[i];
+}
+
+if ((sum % b) == 0)
+{
+    Console.WriteLine("Сумма чисел в массиве кратна числу b");
+}
+else
+{
+    Console.WriteLine("Сумма чисел в массиве не кратна числу b");
+}
